04f2053fd347396f242e75886acd21a9211770e4
tonyf234 <112695036+tonyf234@users.noreply.github.com>
2022-10-07 15:26:47 +0200

    add .env.sample and add .env variable in app

--- src/app.js
+++ src/app.js
@@ -1,16 +1,22 @@
+const path = require('path');
+
 const express = require('express');
 const sequelize = require('./utils/database');
 
 const app = express();
+
+const env_path = path.resolve(__dirname, './../.env');
+require('dotenv').config({ path: env_path });
+console.log('env_path', env_path);
 
 app.use((req, res, next) => {
 	res.send('Hello world!');
 });
 
 sequelize
-	.sync({ force: true })
+	.sync({ force: process.env.NODE_ENV !== 'production' })
 	.then(result => {
-		const PORT = process.env.PORT || 3000
+		const PORT = parseInt(process.env.PORT) || 3000
 		app.listen(PORT);
 		console.log(`app running on port ${PORT}`);
 	})
