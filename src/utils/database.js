f06bb8e7ca34f7c7726fb9edc104fe7a43e36014
tonyf234 <112695036+tonyf234@users.noreply.github.com>
2022-10-07 18:20:32 +0200

    add switch case for the db connection

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,12 +1,24 @@
 const Sequelize = require('sequelize');
+let db = {};
 
-const db = {
-    dialect: 'mysql',
-    host: 'localhost',
-    database: 'schema',
-    user: 'root',
-    password: 'password'
-};
+switch (process.env.NODE_ENV) {
+    case 'development':
+        db.dialect = 'mysql';
+        db.host = 'localhost';
+        db.database = 'schema';
+        db.user = 'root';
+        db.password = 'password'
+        break;
+    case 'test':
+        db.dialect = 'sqlite';
+        // TODO
+        break;
+    case 'production':
+        // TODO
+        break;
+    default:
+        throw new Error('unknown environment');
+}
 
 const sequelize = new Sequelize(db.database, db.user, db.password, { dialect: db.dialect, host: db.host });
 
