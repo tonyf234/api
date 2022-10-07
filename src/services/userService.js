757b074333c96fca3faad38eb309d81eb743257a
yann <yann@vertdyeux.com>
2022-10-07 16:22:17 +0200

    implement createUser deleteUser need make test

--- src/services/userService.js
+++ src/services/userService.js
@@ -11,3 +11,28 @@ exports.findUser = (id) => {
 
     return User.findOne({ where: { id }})
 }
+
+exports.createUser = ({lastname, firstname, email, password}) => {
+    if (typeof firstname !== 'string')
+        throw new TypeError('the firstname must be a string');
+    if (typeof lastname !== 'string')
+        throw new TypeError('the lastname must be a string');
+    if (typeof email !== 'string')
+        throw new TypeError('the email must be a string');
+    if (typeof password !== 'string')
+        throw new TypeError('the password must be a string');
+    const user = User.create({firstname: firstname, lastname: lastname, email: email, password: password});
+    return user.save();
+    }
+
+
+exports.deleteUser = (id) => {
+    if (typeof id !== 'number')
+        throw new TypeError('the id must be a number');
+    const user = findUser(id)
+    if (!user)
+    {
+        throw new TypeError('User not found')
+    }
+    return user.destroy();
+}
