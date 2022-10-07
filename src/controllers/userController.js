d3621e49faa136054574504a1064f36cc3da7a62
tonyf234 <112695036+tonyf234@users.noreply.github.com>
2022-10-07 18:22:30 +0200

    fix getAllUsers method with await in userController

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,5 @@
 const userService = require('../services/userService');
 
-exports.getAllUsers = (req, res, next) => {
-    return res.status(200).send(userService.getAllUsers());
+exports.getAllUsers = async (req, res, next) => {
+    return res.status(200).send(await userService.getAllUsers());
 }
