65a9f4e9aca9d4d49be0dc6aea96806c8f71fa49
tonyf234 <112695036+tonyf234@users.noreply.github.com>
2022-10-11 14:04:27 +0200

    add dotenv var in database.js

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,13 +1,17 @@
 const Sequelize = require('sequelize');
+const path = require("path");
 let db = {};
+
+const env_path = path.resolve(__dirname, './../../.env');
+require('dotenv').config({ path: env_path });
 
 switch (process.env.NODE_ENV) {
     case 'development':
-        db.dialect = 'mysql';
-        db.host = 'localhost';
-        db.database = 'schema';
-        db.user = 'yann';
-        db.password = 'iPhoneSE11'
+        db.dialect = process.env.DEV_DB_DIALECT;
+        db.host = process.env.DEV_DB_HOST;
+        db.database = process.env.DEV_DB_DATABASE;
+        db.user = process.env.DEV_DB_USER;
+        db.password = process.env.DEV_DB_PASSWORD;
         break;
     case 'test':
         db.dialect = 'sqlite';
