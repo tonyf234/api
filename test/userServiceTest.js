40ba34657405585e4e79b4bc4c0e854012eddb38
tonyf234 <112695036+tonyf234@users.noreply.github.com>
2022-10-11 13:47:41 +0200

    add mocha and chai package, add test folder

---
+++ test/userServiceTest.js
@@ -0,0 +1,11 @@
+const userService = require('../src/services/userService');
+
+const expect = require('chai').expect;
+
+it('', () => {
+
+	// const result;
+	// const expected;
+	//
+	// expect(value).to.equal(expected);
+});
